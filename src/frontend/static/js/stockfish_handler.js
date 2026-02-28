660f2b73aa8fb2681977d05d56eb29d621e611c0
Max Amelchenko <maxamel2002@gmail.com>
2026-02-28 16:45:28 +0200

    commit nav cells and stockfish update (#72)

--- src/frontend/static/js/stockfish_handler.js
+++ src/frontend/static/js/stockfish_handler.js
@@ -1,4 +1,5 @@
 var stockfish = null;
+var currentSkillLevel = 5; // Default skill level, updated when user changes it
 
 function stockfish_load() {
   if (!stockfish) {
@@ -10,36 +11,35 @@ function stockfish_load() {
 
 function stockfish_start(fen, rating, customSkillLevel = null) {
   let threads = window.navigator.hardwareConcurrency || 1;
-    var skill_level;
     
-    // Use custom skill level if provided, otherwise derive from rating
-    if (customSkillLevel !== null) {
-      skill_level = customSkillLevel;
-    } else {
-      skill_level = 4;
-    }
+  // Use custom skill level if provided, otherwise use stored level
+  if (customSkillLevel !== null) {
+    currentSkillLevel = customSkillLevel;
+  }
 
-  console.log('Loading stockfish with fen: ' + fen + ' and skill level ' + skill_level);
+  console.log('Loading stockfish with fen: ' + fen + ' and skill level ' + currentSkillLevel);
 
   stockfish.postMessage('uci');
   stockfish.postMessage('ucinewgame');
   stockfish.postMessage(`setoption name Threads value ${threads}`);
-  stockfish.postMessage(`setoption name Skill Level value ${skill_level}`);
+  stockfish.postMessage(`setoption name Skill Level value ${currentSkillLevel}`);
   stockfish.postMessage(`position startpos fen ${fen}`);
   stockfish.postMessage('isready');
 }
 
 function stockfish_move(fen) {
     let movetime = 1000;
+    // Apply the current skill level before each move to ensure it's always set
+    stockfish.postMessage(`setoption name Skill Level value ${currentSkillLevel}`);
     stockfish.postMessage(`position fen ${fen}`);
     stockfish.postMessage(`go movetime ${movetime}`);
 }
 
 function stockfish_set_skill_level(skillLevel) {
-    if (stockfish) {
-        console.log('Updating stockfish skill level to: ' + skillLevel);
-        stockfish.postMessage(`setoption name Skill Level value ${skillLevel}`);
-    }
+    // Just store the skill level, don't send to engine yet
+    // It will be applied on the next move via stockfish_move()
+    currentSkillLevel = skillLevel;
+    console.log('Skill level set to: ' + skillLevel + ' (will apply on next engine move)');
 }
 
 export { stockfish_load, stockfish_move, stockfish_start, stockfish_set_skill_level }
